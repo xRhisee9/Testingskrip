e2c5676dfe037ca94bf3d0e600a56ec72b6772e9
xRhisee9 <falihakhairy14@gmail.com>
2025-03-25 20:54:57 +0700

    Update falih.lua

--- falih.lua
+++ falih.lua
@@ -5,8 +5,8 @@ local function highlightPlayer(player)
     if player.Character then
         local highlight = Instance.new("Highlight")
         highlight.Parent = player.Character
-        highlight.FillColor = Color3.fromRGB(255, 0, 0)
-        highlight.OutlineColor = Color3.fromRGB(0, 0, 255)
+        highlight.FillColor = Color3.fromRGB(255, 0, 0) -- Warna isi
+        highlight.OutlineColor = Color3.fromRGB(0, 0, 255) -- Warna garis luar
     end
 end
 
